dc2f966ed8f1c4ab4937a9e0a04378fbac229d80
jac-tk <117427971+jac-tk@users.noreply.github.com>
2024-01-19 09:13:17 +0900

    update

---
+++ functionKEY.js
@@ -0,0 +1,58 @@
+{/* <script> */}
+    document.addEventListener('keydown', function(event) {
+        switch (event.key) {
+        case 'F1':
+            event.preventDefault();
+            console.log('F1キーを押しました。');
+            break;
+        case 'F2':
+            event.preventDefault();
+            alert('F2キー：更新リロード');
+            console.log('F2キー：更新リロード');
+            break;
+        case 'F3':
+            event.preventDefault();
+            alert('F3キー：前に戻る');
+            console.log('F3キー：前に戻る');
+            break;
+            
+        case 'F4':
+            event.preventDefault();
+            console.log('F4キーを押しました。');
+            break;
+        case 'F5':
+            event.preventDefault();
+            console.log('F5キーを押しました。');
+            break;
+        case 'F6':
+            event.preventDefault();
+            console.log('F6キーを押しました。');
+            break;
+        case 'F7':
+            event.preventDefault();
+            console.log('F7キーを押しました。');
+            break;
+        case 'F8':
+            event.preventDefault();
+            console.log('F8キーを押しました。');
+            break;
+        case 'F9':
+            event.preventDefault();
+            console.log('F9キーを押しました。');
+            break;
+        case 'F10':
+            event.preventDefault();
+            console.log('F210ーを押しました。');
+            break;
+        case 'F11':
+            event.preventDefault();
+            console.log('F211ーを押しました。');
+            break;
+        case 'F12':
+            event.preventDefault();
+            console.log('F212ーを押しました。');
+            break;
+        // event.preventDefault();  // ブラウザのデフォルトの動作をキャンセル
+        }
+    });
+{/* </script> */}
